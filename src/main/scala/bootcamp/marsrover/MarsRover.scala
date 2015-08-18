00102c92ff3c846838f02cfb933ac5cf3dd3dfbf
Anitha <anithapraba@indix.com>
2015-08-18 16:15:52 +0530

    Updated Mars Rover by incorporating functional programming concepts

--- src/main/scala/bootcamp/marsrover/MarsRover.scala
+++ src/main/scala/bootcamp/marsrover/MarsRover.scala
@@ -2,45 +2,32 @@ package com.indix.bootcamp.marsrover
 
 object MarsRover {
 
+  val left: Char = 'L'
+
   val rotateLeftMap: Map[Char,Char] = Map('S'->'E','E'->'N','N'->'W','W'->'S')
   val rotateRightMap: Map[Char,Char] = Map('S'->'W','W'->'N','N'->'E','E'->'S')
 
-  def rotateLeft(char: Char): Char ={
-    rotateLeftMap.get(char).get
+  def moveForward(currentPosition: Set[Int], head: Char): Set[Int] ={
+    head match{
+      case 'N' => Set(currentPosition.head,currentPosition.last + 1)
+      case 'S' => Set(currentPosition.head,currentPosition.last - 1)
+      case 'E' => Set(currentPosition.head + 1,currentPosition.last)
+      case 'W' => Set(currentPosition.head - 1,currentPosition.last)
+    }
   }
 
-  def rotateRight(char: Char): Char ={
-    rotateRightMap.get(char).get
-  }
+  def processRoverMovement(initPosition: Set[Int],initHead: Char, commandList: String): (Set[Int],Char) ={
 
-  def findFinalPosition(command: Char, xx:Int, yy:Int, headc: Char): (Int,Int,Char) = {
-
-    var head: Char = headc
-    var x = xx
-    var y = yy
-
-    command match {
-      case 'L' => head = rotateLeft(head)
-      case 'R' => head = rotateRight(head)
-      case 'M' => head match {
-          case 'N' => (x, y += 1)
-
-          case 'S' => (x, y -= 1)
-
-          case 'E' => (x += 1, y)
-
-          case 'W' => (x -= 1, y)
+    commandList.foldLeft(initPosition,initHead){
+      (position,command) => {
+        command match{
+          case 'L' =>  (position._1,rotateLeftMap(position._2))
+          case 'R' =>  (position._1,rotateRightMap(position._2))
+          case 'M' => (moveForward(position._1,position._2),position._2)
+        }
       }
-
-    }
-    (x,y,head)
-  }
-
-  def processRover(initX: Int,initY: Int,initHead: Char, commandList: String): (Int,Int,Char) ={
-    commandList.toList.foldLeft((initX,initY,initHead)){
-      (position,command) => findFinalPosition(command,initX,initY,initHead)
     }
   }
-
 }
+
 
--- src/test/scala/bootcamp/marsrover/MarsRoverTest.scala
+++ src/test/scala/bootcamp/marsrover/MarsRoverTest.scala
@@ -5,7 +5,8 @@ import org.scalatest.{ShouldMatchers, FlatSpec}
 class MarsRoverTest extends FlatSpec with ShouldMatchers{
 
   "Mars Rover" should "move in the specified direction" in {
-    MarsRover.processRover(1,2,'N',"LMLMLMLMM") shouldBe (1,3,'N')
+    MarsRover.processRoverMovement(Set(1,2),'N',"LMLMLMLMM") shouldBe (Set(1,3),'N')
+    MarsRover.processRoverMovement(Set(3,3),'E',"MMRMMRMRRM") shouldBe (Set(5,1),'E')
   }
 
 }
