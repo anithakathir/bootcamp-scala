1dcfdc6e2d86a0801aabeb20004939e074c95cac
Anitha <anithapraba@indix.com>
2015-08-20 11:42:35 +0530

    Updated SkyCast with case class instance creation each time

--- src/main/scala/bootcamp/remotecontrol/SkyCast.scala
+++ src/main/scala/bootcamp/remotecontrol/SkyCast.scala
@@ -3,7 +3,7 @@ package bootcamp.remotecontrol
 case class SkyCast(minChannel: Int, maxChannel: Int, currentChannel: Int, previousChannel: Int, blocked: List[Int],minClicks: Int) {
 
   def estimateMinClicks(next: Int): SkyCast = {
-    val (current, clicks): (Int, Int) =
+    val (current, clicks) =
       List(
         directClicks(currentChannel, next),
         getNumOfClicks(navigateUp)(currentChannel,next),
@@ -11,28 +11,28 @@ case class SkyCast(minChannel: Int, maxChannel: Int, currentChannel: Int, previo
         getNumOfClicks(navigateUp)(previousChannel,next),
         getNumOfClicks(navigateDown)(previousChannel,next)
       ).minBy(_._2)
-    this.copy(currentChannel = next, previousChannel = current, minClicks = minClicks + clicks)
+    SkyCast(minChannel, maxChannel, currentChannel = next, previousChannel = current, blocked, minClicks = minClicks + clicks)
   }
 
-  def directClicks(currentChannel: Int, nextChannel: Int): (Int,Int) ={
+  def directClicks(currentChannel: Int, nextChannel: Int) ={
     (currentChannel,nextChannel.toString.length)
   }
 
-  def navigateDown(currentChannel: Int, nextChannel: Int): List[Int] = {
+  def navigateDown(currentChannel: Int, nextChannel: Int) ={
     if ( currentChannel <= nextChannel )
       List.range( currentChannel-1, minChannel-1, -1) ++ List.range(maxChannel,nextChannel-1,-1)
     else
       List.range(currentChannel-1,nextChannel-1,-1)
   }
 
-  def navigateUp(currentChannel: Int, nextChannel: Int): List[Int] = {
+  def navigateUp(currentChannel: Int, nextChannel: Int) ={
     if ( currentChannel <= nextChannel )
       List.range(currentChannel+1,nextChannel+1)
     else
       List.range(currentChannel+1,maxChannel+1) ++ List.range(minChannel,nextChannel+1)
   }
 
-  def getNumOfClicks(sequence: (Int,Int) => List[Int])(currentChannel : Int,nextChannel: Int): (Int, Int) = {
+  def getNumOfClicks(sequence: (Int,Int) => List[Int])(currentChannel : Int,nextChannel: Int) ={
     val channelSequence: List[Int] = sequence(currentChannel, nextChannel)
     if(currentChannel == nextChannel)
       (currentChannel,0)
